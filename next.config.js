21117ced904137edf6d851656a34518cd1694746
Charles Muoka <charlesmuoka1@gmail.com>
2022-03-12 14:50:55 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+    env: {
+        MONGO_URI: "mongodb+srv://juanromeroj:juanromeroj@1962@cluster0.li4t7.mongodb.net/Cluster0?retryWrites=true&w=majority"
+        // mongodb+srv://juanromeroj:<password>@cluster0.li4t7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+    }
 }
-
-module.exports = nextConfig
